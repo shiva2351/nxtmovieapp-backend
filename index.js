4e5e96a32f87374537e04ac7f64eccddd3b9679f
shiva251 <smpgr2351@gmail.com>
2025-05-18 16:13:56 +0530

    second

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const movieRoutes = require('./routes/movieRoutes');
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
-app.use('/moives', movieRoutes);
+app.use('/movies', movieRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
